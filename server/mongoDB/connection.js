dba52451dcdd92957012a87177c34f3cf0bfe816
OderoCeasar <156299730+OderoCeasar@users.noreply.github.com>
2025-07-07 13:40:47 +0300

    mongodb connection

---
+++ server/mongoDB/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+    try {
+        mongoose.connect(process.env.URL, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.log("MongoDB connection failed ", error);
+    }
+};
+
+
+export default connectDB;
